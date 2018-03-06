ab40be1a0b36c4a4c1a9da9764990d896a8ae79d
Ronak Baldha <ronak.baldha@gmail.com>
2018-03-06 10:29:09 +0530

    Update baseUrl

--- src/components/common/Utilities.js
+++ src/components/common/Utilities.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:49520/api`
-  // baseURL: `http://innovic.azurewebsites.net/api`
+  // baseURL: `http://localhost:49520/api`
+  baseURL: `http://innovic.azurewebsites.net/api`
   // baseURL: `http://dev-innovic-api.azurewebsites.net/api`
 });
 
