841fc7523054067e399fedc5f77f4c830388062a
Ronak Baldha <ronak.baldha@gmail.com>
2018-03-26 12:34:27 +0530

    Update travis config

--- src/components/common/Utilities.js
+++ src/components/common/Utilities.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:49520/api`,
-  // baseURL: `http://innovic.azurewebsites.net/api`,
-  // baseURL: `http://dev-innovic-api.azurewebsites.net/api`
+  baseURL: process.env.REACT_APP_API,
 });
 
 api.interceptors.request.use(
