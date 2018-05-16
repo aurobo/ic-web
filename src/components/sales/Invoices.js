3420f93ac5a5a11ff8dd01afeb9391d9f7c59efa
Sanket Patel <3sanket3@gmail.com>
2018-05-16 13:07:05 +0530

    update invoice list screen to use ListCell

--- src/components/sales/Invoices.js
+++ src/components/sales/Invoices.js
@@ -1,68 +1,39 @@
-import _ from 'lodash';
 import React from 'react';
-import Table from 'semantic-ui-react/dist/commonjs/collections/Table/Table';
-import ControlPanel from '@innovic/components/shared/ControlPanel';
 import { Link } from 'react-router-dom';
-import TableWithSorting from '@innovic/components/shared/TableWithSorting';
-import Api from '@innovic/components/shared/Api';
+import ListCell from '../shared/anatomy/ListCell';
 
 class Invoices extends React.Component {
-  state = {
-    data: null,
-  };
-
-  handleDataChange = data => {
-    this.setState({ data: data });
-  };
-
-  handleSuccess = data => {
-    this.setState({ data: data });
-  };
-
   render() {
-    const { data } = this.state;
-    return (
-      <Api url="/invoices" onSuccess={this.handleSuccess}>
-        <ControlPanel title="Invoices" />
-        <TableWithSorting sortBy="key" sortIn="desc" data={data} onDataChange={this.handleDataChange}>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell field="key" type="text">
-                Key
-              </Table.HeaderCell>
-              <Table.HeaderCell field="salesOrderKey" type="text">
-                Sales Order
-              </Table.HeaderCell>
-              <Table.HeaderCell field="salesOrderCustomerName" type="text">
-                Customer
-              </Table.HeaderCell>
-              <Table.HeaderCell field="createdOn" type="text">
-                Created On
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {_.map(
-              data,
-              ({ id, key, salesOrderKey, salesOrderId, createdOn, salesOrderCustomerId, salesOrderCustomerName }) => (
-                <Table.Row key={id}>
-                  <Table.Cell selectable>
-                    <Link to={'/sales/invoices/' + id}>{key}</Link>
-                  </Table.Cell>
-                  <Table.Cell selectable>
-                    <Link to={'/sales/sales-orders/' + salesOrderId}>{salesOrderKey}</Link>
-                  </Table.Cell>
-                  <Table.Cell selectable>
-                    <Link to={'/master/customer/' + salesOrderCustomerId + '/view'}>{salesOrderCustomerName}</Link>
-                  </Table.Cell>
-                  <Table.Cell>{new Date(createdOn).toLocaleDateString()}</Table.Cell>
-                </Table.Row>
-              )
-            )}
-          </Table.Body>
-        </TableWithSorting>
-      </Api>
-    );
+    const listTableColumns = [
+      {
+        Header: 'Key',
+        accessor: 'key',
+        Cell: ({ original }) => <Link to={'/sales/invoices/' + original.id}>{original.key}</Link>,
+      },
+      {
+        Header: 'Sales Order',
+        accessor: 'salesOrderKey',
+        Cell: ({ original }) => (
+          <Link to={'/sales/sales-orders/' + original.salesOrderId}>{original.salesOrderKey}</Link>
+        ),
+      },
+      {
+        Header: 'Customer',
+        accessor: 'salesOrderCustomerName',
+        Cell: ({ original }) => (
+          <Link to={'/master/customer/' + original.salesOrderCustomerId + '/view'}>
+            {original.salesOrderCustomerName}
+          </Link>
+        ),
+      },
+      {
+        Header: 'Created On',
+        accessor: 'key',
+        Cell: ({ original }) => <span>{new Date(original.createdOn).toLocaleDateString()}</span>,
+      },
+    ];
+
+    return <ListCell name="Invoices" getUrl="/invoices" tableColumns={listTableColumns} />;
   }
 }
 
