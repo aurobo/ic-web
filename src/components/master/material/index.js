cf55af7b5019ff98e58d4f2792b82717af3c0e55
Ronak Baldha <ronak.baldha@gmail.com>
2018-06-05 10:57:46 +0530

    Add style-guidist

--- src/components/master/material/index.js
+++ src/components/master/material/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import List from './List';
-import { Tissue } from '@innovic/components/shared/anatomy';
-
-class Material extends React.Component {
-  render() {
-    return <Tissue list={List} />;
-  }
-}
-
-export default Material;
+export { default } from './Material';
